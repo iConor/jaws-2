93bc0c6551e0a11b837d9a8f12e1aed76fa4eed4
Conor Cary <cary.42@osu.edu>
2016-04-19 01:11:18 -0400

    Added full set of IMU data transformations.

--- jaws2_navigation/src/quat_to_rpy.cpp
+++ jaws2_navigation/src/quat_to_rpy.cpp
@@ -1,3 +1,4 @@
+
 #include "ros/ros.h"
 #include "tf/transform_datatypes.h"
 #include "tf/LinearMath/Matrix3x3.h"
@@ -10,21 +11,21 @@ geometry_msgs::Vector3Stamped angles;
 
 void callback(const imu_3dm_gx4::FilterOutput::ConstPtr& filter)
 {
-    tf::Quaternion quaternion;
-    tf::quaternionMsgToTF(filter->orientation, quaternion);
-    tf::Matrix3x3 rotation = tf::Matrix3x3(1, 0, 0, 0, -1, 0, 0, 0, -1);
-    tf::Matrix3x3 attitude = rotation * tf::Matrix3x3(quaternion);
-    attitude.getRPY(angles.vector.x, angles.vector.y, angles.vector.z);
-    angles.header.stamp = filter->header.stamp;
-    rpy.publish(angles);
-  }
+  tf::Quaternion quaternion;
+  tf::quaternionMsgToTF(filter->orientation, quaternion);
+  tf::Matrix3x3 rotation = tf::Matrix3x3(1, 0, 0, 0, -1, 0, 0, 0, -1);
+  tf::Matrix3x3 attitude = rotation * tf::Matrix3x3(quaternion);
+  attitude.getRPY(angles.vector.x, angles.vector.y, angles.vector.z);
+  angles.header.stamp = filter->header.stamp;
+  rpy.publish(angles);
+}
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "orientation");
-    ros::NodeHandle nh;
-    imu = nh.subscribe<imu_3dm_gx4::FilterOutput>("imu/filter", 1, callback);
-    rpy = nh.advertise<geometry_msgs::Vector3Stamped>("imu/angles", 1);
-    angles.header.frame_id = "base_link";
-    ros::spin();
+  ros::init(argc, argv, "orientation");
+  ros::NodeHandle nh;
+  imu = nh.subscribe<imu_3dm_gx4::FilterOutput>("imu/filter", 1, callback);
+  rpy = nh.advertise<geometry_msgs::Vector3Stamped>("imu/angles", 1);
+  angles.header.frame_id = "base_link";
+  ros::spin();
 }
